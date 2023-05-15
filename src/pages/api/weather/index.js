2299c937333d7ee93e819773ebc068ec9eb7f5b1
MBPritz <MaybePritz@gmail.com>
2023-05-15 13:53:15 +0300

    Update index.js

--- src/pages/api/weather/index.js
+++ src/pages/api/weather/index.js
@@ -29,7 +29,7 @@ export default async function handler(req, res) {
       })
       .catch(error => res.status(503).json(error))
   } else {
-    const clientIp = req.headers["x-forwarded-for"];
+    const clientIp = '95.67.214.248';
     console.log('IP >>' + clientIp); 
     const geo = await geodecodeIp(clientIp);
     Forecast(`${geo.ll[0]},${geo.ll[1]}`)
