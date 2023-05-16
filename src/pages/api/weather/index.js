5e64be311d86bbd685136b221002a6416489572c
MBPritz <MaybePritz@gmail.com>
2023-05-16 21:27:32 +0300

    2

--- src/pages/api/weather/index.js
+++ src/pages/api/weather/index.js
@@ -1,5 +1,6 @@
 import geodecodeIp from 'micro-geoip-lite';
 import requestIp from 'request-ip';
+import { geolocation } from "@vercel/edge";
 
 async function Forecast(place) {
   const url = `http://api.weatherapi.com/v1/forecast.json?key=${process.env.API_KEY}&q=${place}&days=7&lang=ru`;
@@ -29,10 +30,12 @@ export default async function handler(req, res) {
       })
       .catch(error => res.status(503).json(error))
   } else {
-    // const clientIp = (requestIp.getClientIp(req) !== "::1" ? requestIp.getClientIp(req) : null);
-    // const geo = await geodecodeIp(clientIp);
-    const location = `${geo.ll[0]},${geo.ll[1]}`;
-    Forecast(req.headers['x-vercel-ip-city'])
+    let location = req.nextUrl.searchParams.get("location");
+    if (!location) {
+      const { city } = geolocation(req);
+      location = city || "San Francisco";
+    } 
+    Forecast(location)
       .then(data => {
         if (data) {
           if (res.headersSent !== true) {
